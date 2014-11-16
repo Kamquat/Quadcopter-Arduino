55c83c3174f8b10a1bfb6264576cef96279d777e
Kamquat <naisa_mi_sey@yahoo.com>
2014-11-16 14:54:06 -0800

    Orient with period

--- libraries/Orientation/Orientation.cpp
+++ libraries/Orientation/Orientation.cpp
@@ -4,29 +4,43 @@
 #include "IMUAccess.h"
 #include "math.h"
 
-
-void OrientationTwo::updateOrientation() 
+double Accel_to_G(double);
+void OrientationTwo::updateOrientation()
 {
-	/*double x = IMUAccess.currentAccelValues[0];
-	double y = IMUAccess.currentAccelValues[1];
-	double z = IMUAccess.currentAccelValues[2];*/
-	
-	double ax = Accel_to_G(IMUAccess.currentAccelValues[0]);
-	double ay = Accel_to_G(IMUAccess.currentAccelValues[1]);
-	double az = Accel_to_G(IMUAccess.currentAccelValues[2]);
-	
-	double roll = atan2(-ay,az);
-	double pitch = atan2( ax,(sqrt((ay)*(ay) + (az)*(az))));
-	double thrust = sqrt((ax)*(ax) + (ay)*(ay) + (az)*(az));
+	currentTime = micros();
+	if(currentTime - previousTime > ORIENT_PERIOD)
+	{
+		previousTime = currentTime;
+		
+		double ax = Accel_to_G(IMUAccess.currentAccelValues[0]);
+		double ay = Accel_to_G(IMUAccess.currentAccelValues[1]);
+		double az = Accel_to_G(IMUAccess.currentAccelValues[2]);
+		
+		double roll = atan2(-ay,az);
+		double pitch = atan2( ax,(sqrt((ay)*(ay) + (az)*(az))));
+		double thrust = sqrt((ax)*(ax) + (ay)*(ay) + (az)*(az));
 
-	roll *= 180.00;   pitch *= 180.00;   //zAngle *= 180.00;
-	roll /= 3.141592; pitch /= 3.141592; //zAngle /= 3.141592;
-	
-	xyzDegree[0] = roll;
-	xyzDegree[1] = pitch;
-	xyzDegree[2] = thrust;
-	
-	double Accel_to_G(double x)
+		roll *= 180.00;   pitch *= 180.00;   //zAngle *= 180.00;
+		roll /= 3.141592; pitch /= 3.141592; //zAngle /= 3.141592;
+		
+		xyzDegree[0] = roll;
+		xyzDegree[1] = pitch;
+		xyzDegree[2] = thrust;
+		
+		if(DEBUG == true)
+		{
+		Serial.print("Values:\n");
+		Serial.print("Roll: "); Serial.print(roll);
+		Serial.print("\t");
+		Serial.print("Pitch: "); Serial.print(pitch);
+		Serial.print("\t");
+		Serial.print("Thrust: "); Serial.print(thrust);
+		Serial.print("\n");
+		}
+	}
+}
+
+double Accel_to_G(double x)
 	{
 		double y = 0;
 		double f = (.0016)/(ACCEL_RESOLUTION*((1/.0071)+(1/.0087)));
@@ -43,16 +57,3 @@ void OrientationTwo::updateOrientation()
 	
 		return y;
 	}
-	if(DEBUG == true)
-	{
-	Serial.print("Values:\n");
-	Serial.print("Roll: "); Serial.print(roll);
-	Serial.print("\t");
-	Serial.print("Pitch: "); Serial.print(pitch);
-	Serial.print("\t");
-	Serial.print("Thrust: "); Serial.print(thrust);
-	Serial.print("\n");
-	//delay(2000);
-	}
-	
-}
--- libraries/Orientation/Orientation.h
+++ libraries/Orientation/Orientation.h
@@ -1,12 +1,14 @@
 #ifndef Orientation_h
-
 #define Orientation_h
 #include "Arduino.h"
+
+#define ORIENT_PERIOD 10000 //10,000 microseconds = 100Hz
 
 class OrientationTwo
 {
 	public:
 		void updateOrientation();
+		
 		double xyzDegree[3];
 
 };
